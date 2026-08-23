74497857d857532eeb1ec27125a1f4142c3e583f
oyst12rsas <oyst12rsas@gmail.com>
2026-08-23 08:12:08 +0200

    Deprecate Sleeping Beauty in favor of systemd hotspot service

--- hotspot/perl/sleepingbeauty.pl
+++ hotspot/perl/sleepingbeauty.pl
@@ -1,125 +1,22 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
-use autodie;
-use DBI;
 
-use lib ('/root/taransvar/perl');
-use func;	#NOTE! See comment above regarding lib..
+print STDERR <<'MSG';
+sleepingbeauty.pl is deprecated.
 
-sub sleepingBeautyCount {
-       #NOTE! This function is in both checkSleepingRunning.pl and sleepingBeauty.pl (put in lib file)
-	my @pids = `pgrep -f 'perl .*sleepingbeauty\\.pl'`;
-	chomp @pids;
-	my $nBeautiesFound = scalar @pids;
+Hotspot access control is now managed by systemd:
+  tarasec-hotspot-access.service
 
-	print "Running sleepingbeauty instances: $nBeautiesFound\n";
-	return $nBeautiesFound;
-}
+Hotspot health/DHCP checks are managed by:
+  tarasec-hotspot-watch.timer
 
-#my $szSysRoot = "/home/setup/";
-my $szSysRoot = "/root/wifi/";
+Install/update the services with:
+  sudo bash misc/setup_node_app_services.sh
 
-my $szTempFile = $szSysRoot."temp/temp.txt";
-my $szLogFile = $szSysRoot."log/sleeping.txt";
+Inspect the access controller with:
+  sudo systemctl status tarasec-hotspot-access.service
+  sudo journalctl -u tarasec-hotspot-access.service -n 100 --no-pager
+MSG
 
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-
-open(my $fLogH, '>', $szLogFile) or die "Could not open log file '$szLogFile' $!";
-print $fLogH "sleepingbeauty started $hour:$min:$sec and stopped immediately because there's no hotspot - changing iptables causes trouble for cyber system\n";
-exit;
-
-#my @pids = `pgrep -f 'perl .*sleepingbeauty\\.pl'`;
-#chomp @pids;
-#my $nBeautiesFound = scalar @pids;
-
-#print "Running sleepingbeauty instances: $nBeautiesFound\n";
-my $nBeautiesFound = sleepingBeautyCount();
-
-if ($nBeautiesFound > 3)
-{
-    my $szTxt = "$nBeautiesFound beauties found (including this one)... exiting...\n";
-	print $szTxt;
-    print $fLogH $szTxt;
-
-	exit;
-} else {
-    my $szTxt = "$nBeautiesFound beauty found (this one)... continuing...\n";
-	print $szTxt;
-    print $fLogH $szTxt;
-}
-
-close $fLogH;
-
-while (1)
-{
-	print "Calling getConnection()\n";
-	my $dbh = getConnection();
-	print "After called\n";
-
-	#Read setup...
-
-	my $sth = $dbh->prepare("select CAST(requiresAccessUpdate AS UNSIGNED) as requiresAccessUpdate, if(now() > date_add(lastAccessUpdate, INTERVAL 2 MINUTE),1,0) as TimeToUpdate, coalesce(maintenanceRequest,'') as maintenanceRequest from hotspotSetup limit 1")
-		or die "prepare statement failed: $dbh->errstr()";
-	$sth->execute() or die "execution failed: $dbh->errstr()";
-	if (my $cSetup = $sth->fetchrow_hashref()) 
-	{
-		my $bRequiresUpdate  = $cSetup->{'requiresAccessUpdate'};
-		my $bTimeToUpdate = $cSetup->{'TimeToUpdate'};
-        	my $szSQL;
-		
-		if ($bRequiresUpdate + $bTimeToUpdate) {
-			system("perl ".$szSysRoot."perl/readfwrules.pl");
-			system("perl ".$szSysRoot."perl/db.pl >> ".$szSysRoot."log/cronlog.txt");
-
-			$szSQL = "update hotspotSetup set requiresAccessUpdate = b'0', lastAccessUpdate = now(), lastAccessUpdatePoll = now()";
-
-
-        		my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-
-			open(my $fLogH, '>>', $szLogFile) or die "Could not open log file '$szLogFile' $!";
-			print $fLogH "Access updated $hour:$min:$sec\n";
-			close $fLogH;
-
-		} else {
-			$szSQL = "update hotspotSetup set lastAccessUpdatePoll = now()";
-		}
-
-		my $hUpdate = $dbh->prepare($szSQL) or die "prepare statement failed: $dbh->errstr()";
-		$hUpdate->execute() or die "execution failed: $dbh->errstr()";
-		$hUpdate->finish();
-
-		
-		if ($cSetup->{'maintenanceRequest'} ne '')
-		{
-			my $szRequest = $cSetup->{'maintenanceRequest'};
-			print "**** Maintenance request found: $szRequest Processing....\n";
-
-			if ($szRequest eq "setup_network")
-			{
-				system("perl ".$szSysRoot."perl/setup_network.pl > /var/www/html/temp/maintenanceRequest.txt");
-			} else {
-				if ($szRequest eq "debugserver")
-				{
-					system("perl ".$szSysRoot."perl/syssnapshot.pl > /var/www/html/temp/maintenanceRequest.txt");
-				}
-			}
-
-			my $hUpdate = $dbh->prepare("update setup set maintenanceRequest = null")
-				or die "prepare statement failed: $dbh->errstr()";
-
-			$hUpdate->execute() or die "execution failed: $dbh->errstr()";
-			$hUpdate->finish();
-			print "**** Finished processing maintenance request....\n";
-
-			my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-			open(my $fLogH, '>>', $szLogFile) or die "Could not open log file '$szLogFile' $!";
-			print $fLogH "Maintenance request handled $hour:$min:$sec: ".$cSetup->{'maintenanceRequest'}."\n";
-			close $fLogH;
-		}
-	}
-
-	$sth->finish();
-	$dbh->disconnect;
-
-	sleep(5);
-}
+exit 0;
