a7c0debb344fae4a5860a53921cbdfe85053c39d
oyst12rsas <oyst12rsas@gmail.com>
2026-08-26 11:59:08 +0200

    Support legacy hotspot users when adding time

--- html/hotspot/xxx/users_addtime.php
+++ html/hotspot/xxx/users_addtime.php
@@ -9,7 +9,7 @@ function users_addtime()
 	$pDb = new CDb;
 	
 	$cFlds = array(":name"=>request("name"));
-	if (!$cFetched = $pDb->fetchNext("select expirytime from radcheck where username = :name and op = ':=' and attribute = 'Cleartext-Password'", $cFlds))
+	if (!$cFetched = $pDb->fetchNext("select expirytime from radcheck where username = :name and ((op = ':=' and attribute = 'Cleartext-Password') or (op = '==' and coalesce(attribute,'') = ''))", $cFlds))
 	{
 		print "User not found! Aborting.";
 		return;
@@ -22,59 +22,23 @@ function users_addtime()
 		$szCurrent = $szNow;
 
 	$timestamp = strtotime($szNow) + 60*60;
-	
 	$szOneHour = date('Y-m-d H:i', $timestamp);
-
 	$cParts = explode(":",$szOneHour);
-
 	$szEndOfHour = $cParts[0].":"."00";
-
 	$timestamp = strtotime($szNow) + 60*60*24;
 	$sz24Hours = date('Y-m-d H:i', $timestamp);
-
 	$szSetTimeTo = "";
-
 	$timestamp = strtotime($szCurrent) + 60*60;
 	$szAdd1Hour = date('Y-m-d H:i', $timestamp);
-
 	$timestamp = strtotime($szCurrent) + 60*60*24;
 	$szAdd24Hours = date('Y-m-d H:i', $timestamp);
 
-	if (isset($_POST["now"]))
-	{
-		$szSetTimeTo = $szNow;
-		unset($_POST["now"]);
-	}
-
-	if (isset($_POST["hourend"]))
-	{
-		$szSetTimeTo = $szEndOfHour;
-		unset($_POST["hourend"]);
-	}
-
-	if (isset($_POST["onehour"]))
-	{
-		$szSetTimeTo = $szOneHour;
-		unset($_POST["onehour"]);
-	}
-
-	if (isset($_POST["24hours"]))
-	{
-		$szSetTimeTo = $sz24Hours;
-		unset($_POST["24hours"]);
-	}
-
-	if (isset($_POST["add1hour"]))
-	{
-		$szSetTimeTo = $szAdd1Hour;
-		unset($_POST["add1hour"]);
-	}
-
-	if (isset($_POST["add24hours"]))
-	{
-		$szSetTimeTo = $szAdd24Hours;
-		unset($_POST["add24hours"]);
-	}
+	if (isset($_POST["now"])) { $szSetTimeTo = $szNow; unset($_POST["now"]); }
+	if (isset($_POST["hourend"])) { $szSetTimeTo = $szEndOfHour; unset($_POST["hourend"]); }
+	if (isset($_POST["onehour"])) { $szSetTimeTo = $szOneHour; unset($_POST["onehour"]); }
+	if (isset($_POST["24hours"])) { $szSetTimeTo = $sz24Hours; unset($_POST["24hours"]); }
+	if (isset($_POST["add1hour"])) { $szSetTimeTo = $szAdd1Hour; unset($_POST["add1hour"]); }
+	if (isset($_POST["add24hours"])) { $szSetTimeTo = $szAdd24Hours; unset($_POST["add24hours"]); }
 	
 	if (isset($_POST["submitTime"]))
 	{
@@ -88,8 +52,8 @@ function users_addtime()
 		$cFlds = array(":name"=>request("name"), ":expiry"=>$szSetTimeTo);
 		$pDb->execute("update radcheck set expirytime = :expiry where username = :name", $cFlds);
 		requireAccessUpdate();
-		users_addtime();	//Old name: setSubdcriptionEndTime();
-		return; //Read the current data again...
+		users_addtime();
+		return;
 	}
 	
 	$szRows = tr(td("Set subscription expiry time:",2));
@@ -101,15 +65,8 @@ function users_addtime()
 	$szRows .= tr(td('<button  type="submit" name="24hours">24 hours</button>').td($sz24Hours));
 	$szRows .= tr(td('<button  type="submit" name="add1hour">+ 1 hour</button>').td($szAdd1Hour));
 	$szRows .= tr(td('<button  type="submit" name="add24hours">+ 24 hours</button>').td($szAdd24Hours));
-
 	$szRows .= tr(td('<input name="setTo" value="'.$szCurrent.'" width="5"><button  type="submit" name="submitTime">Submit time</button>',2));
-
-	//$szRows .= tr(td('<button type="submit">Submit</button>',2));					
-	//$szRows .= tr(td("You may have received confirmation code to email or SMS depending on the current policy.",2));					
-	//print '<form  action="index.php?f=main_confCode&name='.$szName.'&pass='.$szPass.'" method="post">'.table($szRows).'</form>';
 	print '<form  action="index.php?f=users_addtime&name='.request("name").'" method="post">'.table($szRows).'</form>';
-
-
 }
 
 ?>
