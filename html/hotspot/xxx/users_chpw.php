44e9c1beec377e5597ef8e4ae7e4a26d3fa334a9
oyst12rsas <oyst12rsas@gmail.com>
2026-08-31 12:40:12 +0200

    Update hotspot subscriber password from back office

--- html/hotspot/xxx/users_chpw.php
+++ html/hotspot/xxx/users_chpw.php
@@ -1,6 +1,5 @@
 <?php
 
-//function showUser()
 function users_chpw()
 {
 	if (!isSuperUser())
@@ -10,44 +9,44 @@ function users_chpw()
 	}
 
 	$szUser = request("nm");
-	
-	print "Change password for $szUser<br><br>";
+	$szUserEsc = htmlspecialchars($szUser, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+	print "Change password for $szUserEsc<br><br>";
 
+	$pDb = new CDb;
+	$cUser = $pDb->fetch("select username from hotspotSubscriber where username=:name limit 1", array(":name"=>$szUser));
+	if (!$cUser)
+	{
+		print "User not found! Aborting.";
+		return;
+	}
+
+	$szPwd1 = "";
+	$szPwd2 = "";
 	if (isset($_POST["submit"]))
 	{
-		$szPwd1 = request("pwd");
-		$szPwd2 = request("pwd2");
+		$szPwd1 = (string)request("pwd");
+		$szPwd2 = (string)request("pwd2");
 
-		if (strcmp($szPwd1, $szPwd2))
-		{
+		if ($szPwd1 === "")
+			print "<br>".red("PASSWORD MAY NOT BE EMPTY!")."<br><br>";
+		else if (!hash_equals($szPwd1, $szPwd2))
 			print "<br>".red("THE PASSWORDS DIFFER!")."<br><br>";
-		}
 		else
 		{
-			//print "<br>".red("NOT YET LEARNED TO SAVE")."<br><br>";
-			$pDb = new CDb;
-			$cFlds = array(":name" => $szUser);
-			$szSQL = "update radcheck set value = ? where username = ? and op = ':=' and attribute = 'Cleartext-Password'";
-			$stmt = $conn->prepare($szSQL);
-			$stmt->bind_param("ss", $szPwd1, $szUser);
-	                $stmt->execute();
-			print 'Password is changed.<br><br><a href="index.php?f=users_show&nm='.$szUser.'">Back to user</a>';
+			$cFlds = array(":name"=>$szUser, ":password"=>$szPwd1);
+			$pDb->execute("update hotspotSubscriber set password=:password where username=:name", $cFlds);
+			// Keep the explicit legacy FreeRADIUS password row synchronized.
+			$pDb->execute("update radcheck set value=:password where username=:name and op=':=' and attribute='Cleartext-Password'", $cFlds);
+			print 'Password is changed.<br><br><a href="index.php?f=users_show&nm='.rawurlencode($szUser).'">Back to user</a>';
 			return;
 		}
-	} 
-	else
-	{
-		$szPwd1 = "";
-		$szPwd2 = "";
 	}
-	
-			
-	print '<form  action="index.php?f=users_chpw&nm='.$szUser.'" method="post">';
-	$szRows = tr(td('Password:').td('<input type="edit" name="pwd" value="'.$szPwd1.'">')).
-	tr(td('Repeat password:').td('<input type="edit" name="pwd2" value="'.$szPwd2.'">>')).
-	tr(td('<button type="submit" name="submit">Submit</button>',2));
-	
-	print table($szRows);
+
+	print '<form action="index.php?f=users_chpw&nm='.rawurlencode($szUser).'" method="post">';
+	$szRows = tr(td('Password:').td('<input type="password" name="pwd" value="">')).
+		tr(td('Repeat password:').td('<input type="password" name="pwd2" value="">')).
+		tr(td('<button type="submit" name="submit">Submit</button>',2));
+	print table($szRows).'</form>';
 }
 
 ?>
