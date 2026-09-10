b92a90233fa2c4b9f7aaf2ac36c206b7283f71d8
oyst12rsas <oyst12rsas@gmail.com>
2026-09-10 04:51:33 +0200

    Serialize traffic rejection outcome

--- tarakernel/module_timed_operations.c
+++ tarakernel/module_timed_operations.c
@@ -61,7 +61,6 @@ bool getTrafficReport(char *lpSendBuf, int bufSize);
 bool getTrafficReport(char *lpSendBuf, int bufSize)
 {
     pSetup->bTrafficReportsBeingHandled = true;
-	//memset(lpSendBuf, 0, N_SENDBUF_SIZE);
 	strcpy(lpSendBuf, C_TRAFFIC_REPORT_PREFIX);
 
     int n;
@@ -72,12 +71,18 @@ bool getTrafficReport(char *lpSendBuf, int bufSize)
             break;
                 
         struct _ipPort2 *pRec = &pSetup->cPendingIncomingReportArr[n];
-        //OT_Changed:260225 - now also sending the tag...
-        //sprintf(lpSendBuf+strlen(lpSendBuf), "%08X-%X-%X-%X^", swappedEndian(pRec->ip), pRec->sPort, pRec->dPort, pRec->nCount);
-        //ØT 260305 - Here's where sending...
         char cThisNode[150];
 
-        snprintf(cThisNode, sizeof(cThisNode), "%08X-%X-%08X-%X-%X-%X^", swappedEndian(pRec->sIp), pRec->sPort, swappedEndian(pRec->dIp), pRec->dPort, pRec->nCount, pRec->nTag);
+        /*
+         * Seventh field is the traffic action. Legacy reports had six fields;
+         * 0 means ordinary observed/accepted traffic and 1 means tarakernel
+         * rejected the packet. The gateway does not know whether a flow is a
+         * demo; dbserver is responsible for correlating it with demo state.
+         */
+        snprintf(cThisNode, sizeof(cThisNode), "%08X-%X-%08X-%X-%X-%X-%X^",
+                 swappedEndian(pRec->sIp), pRec->sPort,
+                 swappedEndian(pRec->dIp), pRec->dPort,
+                 pRec->nCount, pRec->nTag, pRec->nAction);
 
         int nMaxWrite = N_SENDBUF_SIZE - strlen(lpSendBuf);
 
@@ -89,70 +94,33 @@ bool getTrafficReport(char *lpSendBuf, int bufSize)
 
 		strcpy(lpSendBuf+strlen(lpSendBuf), cThisNode);
 
-		//Clear this spot so it can be used for next report...
-        pSetup->cPendingIncomingReportArr[n].sIp = 0;  
-        pSetup->cPendingIncomingReportArr[n].dIp = 0;   //Not necessary....
+        memset(&pSetup->cPendingIncomingReportArr[n], 0,
+               sizeof(pSetup->cPendingIncomingReportArr[n]));
     }
         
     if (n == 0)
 	{
 
-    	if (pSetup->cShowInstructions.bits.doReportTraffic) //NOTE Never gets her if this is set because checking first in the function (including here is the other deleted)
+    	if (pSetup->cShowInstructions.bits.doReportTraffic)
     	    if (pSetup->cShowInstructions.bits.showOther)
                 pr_info("tarakernel: No traffic to report to taralink...\n");
-        //kfree(lpSendBuf); - don't kfree here... freed by caller...
         pSetup->bTrafficReportsBeingHandled = false;
-        return 0;   //Means didn't find traffic
+        return 0;
     }
-
-	//Used to copy all to the buffer for sending before clearing... But now clearing just after the data put in buffer (see end of for loop above) 
-	//memset(pSetup->cPendingIncomingReportArr, 0, sizeof(pSetup->cPendingIncomingReportArr));
 
     strcpy(lpSendBuf+strlen(lpSendBuf), "EOF");
     pSetup->bTrafficReportsBeingHandled = false;
-	return true;    //Means found traffic
+	return true;
 }
 
 
 bool trafficReportToTaralinkFound(int nProcessId)
 {
-  //  pr_info("tarakernel: ************* Dropping sending traffic report upon request by taralink. Implementing sending at own will..\n");
     return false;
-
-/*    if (pSetup->bTrafficReportsBeingHandled)
-    {
-        pr_info("tarakernel: ************* Dropping sending traffic report (requested by taralink) because already being handled..\n");
-        return false;
-    }
-    pSetup->bTrafficReportsBeingHandled = true;
-    
-    if (!pSetup->cShowInstructions.bits.doReportTraffic)  //No need to do anything further if not supposed to report traffic...
-    {
-        pSetup->cPendingIncomingReportArr[0].sIp = 0;   //To avoid that one with wrong time is being inserte in table if reporting is being turend on later. 
-        pSetup->cPendingIncomingReportArr[0].dIp = 0;
-        return false;
-    }
-
-    char *lpSendBuf = kmalloc(N_SENDBUF_SIZE, GFP_KERNEL);
-        
-    if (!lpSendBuf)
-    {
-        pr_info("tarakernel: **** ERROR ****** Couldn't allocate buffer in trafficReportTotaralinkFound()\n");
-        return 0;
-    }
-
-    getTrafficReport(lpSendBuf, N_SENDBUF_SIZE);
-	
-	//Send traffic message to taralink. 
-    sendMessage(nProcessId, lpSendBuf);
-    kfree(lpSendBuf);
-    return 1;
-*/    
 }
 
 void sendCheckRequests(int nProcessId)
 {
-    //For now only sending requests to check partner and IP setup after finding forwarded traffic where neither sender or receiver is own IP..
     char *lpSendBuf = kmalloc(N_SENDBUF_SIZE, GFP_KERNEL);
         
     int n;
@@ -168,7 +136,7 @@ void sendCheckRequests(int nProcessId)
         if (nLen + 20 > N_SENDBUF_SIZE)
         {
             pr_info("tarakernel: ***** ERROR *** Buffer for traffic to check is too small (weird that gets here)...\n");
-            break;  //Shouldn't get here... Error message just in case...
+            break;
         }
                 
         char *lpCheckWhat = (pSetup->cCheckThese[n].eCheckType == e_PossiblePartner? "partner?":"???");
@@ -177,7 +145,6 @@ void sendCheckRequests(int nProcessId)
 
     if (n)
     {
-        //Clean up the array for new check requests
 	    memset(pSetup->cCheckThese, 0, sizeof(pSetup->cCheckThese));
 
         sendMessage(nProcessId, lpSendBuf);
@@ -199,23 +166,15 @@ void sendTrafficReport()
 
     if (getTrafficReport(lpSendBuf, N_SENDBUF_SIZE))
     {
-        //pr_warn("tarakernel: About to send: %s\n", lpSendBuf);
     	send_to_user(lpSendBuf);
     }
 
-    //sendMessage(nProcessId, lpSendBuf);   this is for sending when being requested....
 	kfree(lpSendBuf);
     pSetup->bSendTrafficReport = false;     
 }
 
 void checkTimedOperation(void)
 {
-    /*
-     * Flush queued observations on the one-second kernel timer. Previously,
-     * ordinary (including clean) traffic waited about six ticks, while a
-     * changed threat severity set bSendTrafficReport for the next tick.
-     * Nonzero tags can also re-arm this timer immediately.
-     */
     if (pSetup->bSendTrafficReport ||
         pSetup->cPendingIncomingReportArr[0].sIp)
     {
@@ -223,26 +182,18 @@ void checkTimedOperation(void)
         return;
     }
 
-    //pr_warn("tarakernel: Timed operations...\n");
-
 /*
     241230 - probably not working.... checkRequestForStatus() is called with process id 0 below and such messages are never being sent... 
               checkRequestForStatus() is being called from hello_nl_recv_msg, so that's where it happens....
-        
-  NOTE! Commenting it out may change things, though, because pSetup->nLastTimedOperation is altered....
 
     if (pSetup->nLastTimedOperation)
     {
       u64 nCurrentTimestamp = ktime_get_coarse_real_ns();
       int nSecondsLapsed = (nCurrentTimestamp - pSetup->nLastTimedOperation) / (1000 * 1000 * 1000);
       
-      //printk(KERN_INFO "tarakernel: %llu nanosec..\n", nCurrentTimestamp - nLastTimedOperation);
-      
       if (nSecondsLapsed > 60)
       {
         pSetup->nLastTimedOperation = nCurrentTimestamp; 
-       // printk(KERN_INFO "tarakernel: %d seconds lapsed.. Consider sending status..\n", nSecondsLapsed);
-        //NOTE! Probably never sends request because checkRequestForStatus only sends if processId > 0 (and that's 1st param)
         checkRequestForStatus(0, "dummy");
       }
     }
