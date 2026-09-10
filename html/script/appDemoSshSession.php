2520bd99fd73bf97c8fe290373764f03d6ac9dc3
oyst12rsas <oyst12rsas@gmail.com>
2026-09-10 12:56:22 +0200

    Insert Demo 2 expiry with TIMESTAMPADD

--- html/script/appDemoSshSession.php
+++ html/script/appDemoSshSession.php
@@ -121,7 +121,7 @@ try {
         $unitRow = $stmt->get_result()->fetch_assoc();
         $stmt->close();
         if ($unitRow) $unitId = (int)$unitRow['unitId'];
-        $stmt = $conn->prepare("INSERT INTO demoSshSession(demoSshSetupId,demoSshNodeBId,sourceIp,unitId,credentialGeneration,accessTokenHash,expires) VALUES(?,?,INET_ATON(?),?,?,?,DATE_ADD(NOW(),INTERVAL ? SECOND))");
+        $stmt = $conn->prepare("INSERT INTO demoSshSession(demoSshSetupId,demoSshNodeBId,sourceIp,unitId,credentialGeneration,accessTokenHash,expires) VALUES(?,?,INET_ATON(?),?,?,?,TIMESTAMPADD(SECOND,?,NOW()))");
         $stmt->bind_param('iisiisi', $setupId, $node['demoSshNodeBId'], $sender, $unitId, $node['credentialGeneration'], $accessHash, $ttl);
         $stmt->execute();
         $sessionId = (int)$conn->insert_id;
